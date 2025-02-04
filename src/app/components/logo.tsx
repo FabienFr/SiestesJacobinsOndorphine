d2f7eac948fc4f1f09587ca1d36c33ab3bb68010
FabienFr <fabien@xaeos.com>
2025-02-04 13:55:24 +0100

    new white logo responsive

--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -2,28 +2,38 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
+import styled from '@emotion/styled';
 
 export default function Logo() {
   const [hasError, setHasError] = useState(false);
 
   if (hasError) return null;
+  const StyledDiv = styled.div`
+    position: fixed;
+    z-index: 9999;
+    @media (min-width: 768px) {
+      top: 2rem;
+      right: 2rem;
+      transform: none;
+    }
+    @media (max-width: 767px) {
+      bottom: 2rem;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  `;
 
   return (
-    <div className="fixed bottom-8 right-8 z-[9999] pointer-events-non" style={{ 
-        position: 'fixed',
-        top: '2rem',
-        right: '2rem',
-        zIndex: 9999 
-    }}
-    >
+    <StyledDiv>
       <Image 
         src="/images/logo2.png" 
         alt="Logo"
         width={250}
         height={250}
+        className="w-[120px] h-[120px] md:w-[250px] md:h-[250px]"
         priority
         onError={() => setHasError(true)}
       />
-    </div>
+    </StyledDiv>
   );
 }
